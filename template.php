5eb07d7f123b7f50f86b87495718bfe6d304c8fa
Stephan Maximilian Huber <stephan@factorial.io>
2015-07-17 19:08:30 +0200

    Render fields w/o markup and wrap fedit around

--- template.php
+++ template.php
@@ -16,3 +16,20 @@ require_once __DIR__ . '/includes/pager.inc';
 require_once __DIR__ . '/includes/theme.inc';
 require_once __DIR__ . '/includes/node.inc';
 require_once __DIR__ . '/includes/forms.inc';
+require_once __DIR__ . '/includes/field.inc';
+
+
+function frontend_fedit($content, $entity_type, $entity_bundle, $entity_id, $name, $html) {
+  $output = is_array($content) ? drupal_render($content) : $content;
+  if (!user_access('access fedit')) {
+    return $output;
+  }
+  $data = fedit_helper_generate_data($entity_type, $entity_bundle, $entity_id, $name, $html);
+  $attrs = array(
+    'id' => $data['id'],
+    'data-options' => json_encode($data),
+    'class' => 'js-Fedit',
+  );
+  return '<div ' . drupal_attributes($attrs). '>' . $output . '</div>';
+}
+
